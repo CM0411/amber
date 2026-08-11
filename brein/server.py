07f7eefe27a32d336b1674d0705faf3b058c5a51
Cley <cleyvandervliet@gmail.com>
2026-08-11 23:02:21 +0200

    venster: 10 Hz-tikker op teller, balk en resttijd (extrapolatie op gemeten tempo, elke seconde hersynchroniseerd; server peilt elke 2 s)

--- brein/server.py
+++ brein/server.py
@@ -119,7 +119,7 @@ def _ververs_run():
                 _run["scores"] = proefwerk_scores
                 _run["config"] = config
                 _run["tijd"] = time.time()
-        time.sleep(5)
+        time.sleep(2)
 
 
 class Venster(BaseHTTPRequestHandler):
